acec4393c8b9b938802b058c055edbcd014f61d4
azzamsa <azzam@azzamsa.com>
2019-02-15 14:30:42 +0700

    Refactor PersonPopup to student_view

---
+++ Views/student_view.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/home/azzamsya/code-coba-home/skripsi/Lupv/Resources/ui/student_view.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.name_label = QtWidgets.QLabel(Form)
+        self.name_label.setObjectName("name_label")
+        self.verticalLayout.addWidget(self.name_label)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.name_label.setText(_translate("Form", "TextLabel"))
+

8126edca7e30ef22f160ab440ab73c1977f01751
azzamsa <azzam@azzamsa.com>
2019-02-15 20:23:13 +0700

    Change WindowTitle to Lupv

--- Views/main_window.py
+++ Views/main_window.py
@@ -77,7 +77,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Lupv"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "Nama"))
         item = self.tableWidget.horizontalHeaderItem(1)

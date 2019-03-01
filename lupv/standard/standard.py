ac73fc5fd66fa0ec55878464d7dec31c800876a5
azzamsa <azzam@azzamsa.com>
2019-03-01 08:10:34 +0700

    Use my own MyComboBox

--- lupv/standard/standard.py
+++ lupv/standard/standard.py
@@ -1,7 +1,16 @@
 from collections import OrderedDict
+from PyQt5 import QtCore, QtWidgets
 
 
 class MyDict(OrderedDict):
     def __missing__(self, key):
         val = self[key] = MyDict()
         return val
+
+
+class MyComboBox(QtWidgets.QComboBox):
+    popupAboutToBeShown = QtCore.pyqtSignal()
+
+    def showPopup(self):
+        self.popupAboutToBeShown.emit()
+        super(MyComboBox, self).showPopup()
